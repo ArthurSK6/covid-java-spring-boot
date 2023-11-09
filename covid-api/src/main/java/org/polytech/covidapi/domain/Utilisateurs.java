77ce992ee5f82562910bc7251d03d7e74aaf812a
Ocin57 <nicolas.kieger.contact@gmail.com>
2023-11-09 20:50:34 +0100

    mise en place des variables avec getter et setter + Tostring

--- covid-api/src/main/java/org/polytech/covidapi/domain/Utilisateurs.java
+++ covid-api/src/main/java/org/polytech/covidapi/domain/Utilisateurs.java
@@ -1,5 +1,94 @@
 package org.polytech.covidapi.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Création d'une table Utilisateurs avec les attributs id, nom, prenom, email, password, role
+
+@Entity
+@Table
 public class Utilisateurs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
+    private String role;
+
+    public Utilisateurs() {
+    }
+
+    public Utilisateurs(String nom, String prenom, String email, String password, String role) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
     
+    // Getters et Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Utilisateurs [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", password="
+                + password + ", role=" + role + "]";
+    }
+   
+   
 }
